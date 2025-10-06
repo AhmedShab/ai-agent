0355a550f2b847e5ae4491e870c15a87362778f7
Ahmed Shaaban <hello.ahmed.sh@gmail.com>
2025-10-06 16:35:31 +0300

    run py

--- tests.py
+++ tests.py
@@ -1,12 +1,17 @@
-from functions.get_file_content import get_file_content
-from functions.get_files_info import get_files_info
-from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
+# run_python_file("calculator", "main.py") (should print the calculator's usage instructions)
+# run_python_file("calculator", "main.py", ["3 + 5"]) (should run the calculator... which gives a kinda nasty rendered result)
+# run_python_file("calculator", "tests.py")
+# run_python_file("calculator", "../main.py") (this should return an error)
+# run_python_file("calculator", "nonexistent.py") (this should return an error)
 
 def main():
     working_dir = "calculator"
-    print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
-    print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-    print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed"))
+    print(run_python_file(working_dir, "main.py"))
+    print(run_python_file(working_dir, "main.py", ["3 + 5"]))
+    print(run_python_file(working_dir, "tests.py"))
+    print(run_python_file(working_dir, "../main.py"))
+    print(run_python_file(working_dir, "nonexistent.py"))
 
 main()
