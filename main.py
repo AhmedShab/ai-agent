4b716eb3a8d13273976467647c89f14d3da831e6
Ahmed Shaaban <hello.ahmed.sh@gmail.com>
2025-10-07 16:04:01 +0300

    Function Declaration

--- main.py
+++ main.py
@@ -3,12 +3,23 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import get_files_info
+from functions.schema_get_files_info import schema_get_files_info
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    system_prompt = """
+                        You are a helpful AI coding agent.
+
+                        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+                        - List files and directories
+
+                        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+                    """
 
     if len(sys.argv) < 2:
         print("I need a prompt")
@@ -23,20 +34,40 @@ def main():
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
 
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents= messages
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
     )
 
-    print(response.text)
-    if (response is None or response.usage_metadata is None):
+    config = types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=config,
+    )
+
+    if response is None or response.usage_metadata is None:
         print("no response or usage metadata")
         return
+
     if verbose_flage:
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-# main()
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(
+                f"Calling function: {function_call_part.name}({function_call_part.args})"
+            )
+    else:
+        print(response.text)
 
-print(get_files_info("calculator", "../"))
+
+main()
+
+# print(get_files_info("calculator", "../"))
